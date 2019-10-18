429d60924e3a42038d4f9628d0b0fb62a79b0a2c
Vincent Pilotte <vpilotte@MACPRO-VPILOTTE-2.local>
2019-10-18 10:11:48 -0400

    second commit

--- weather.py
+++ weather.py
@@ -3,7 +3,6 @@ import feedparser
 import signal 
 import sys
 import html2text
-import speech_recognition as sr
 from threading import Thread
 
 app_running = True
@@ -38,16 +37,5 @@ while app_running:
 		#engine.say(feed.entries[x].title)
 		#engine.say(html2text.html2text(feed.entries[x].summary))
 		#engine.run()
-
-	r = sr.Recognizer()
-	with sr.Microphone() as source:
-		print("Say something!")
-		audio = r.listen(source)
-		try:
-			print("Google Speech Recognition thinks you said " + r.recognize_google(audio, language='fr-CA'))
-		except sr.UnknownValueError:
-			print("Google Speech Recognition could not understand audio")
-		except sr.RequestError as e:
-			print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 	app_running = False
